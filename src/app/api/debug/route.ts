aac6971aeb58ee8734a755dcedf4333a4b89ddf0
Johan Palsterman <johan.palsterman@gmail.com>
2026-01-01 20:35:24 +0100

    Add debug route

---
+++ src/app/api/debug/route.ts
@@ -0,0 +1,9 @@
+﻿import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.json({
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID ? 'SET' : 'UNDEFINED',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'UNDEFINED',
+    NODE_ENV: process.env.NODE_ENV || 'UNDEFINED',
+  });
+}
